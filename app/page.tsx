1aeef5595a7a68c959502777052d223988ea0d4b
Ben Borgers <borgersbenjamin@gmail.com>
2022-11-21 21:26:11 -0500

    Contact form

--- app/page.tsx
+++ app/page.tsx
@@ -31,7 +31,7 @@ export default async function Home() {
 
         <div className="mt-10">
           <Link
-            href="/"
+            href="/contact"
             className={classNames(
               "block border border-gray-500 px-4 py-2 rounded-full text-gray-50 bg-gray-900",
               "hover:bg-gray-50 hover:text-gray-900 hover:border-gray-50 transition-colors duration-200",
