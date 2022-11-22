286f422fe234a8c8bfe439c1dbb4bc1537d64ad5
Ben Borgers <borgersbenjamin@gmail.com>
2022-11-21 21:14:31 -0500

    Add commission button

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,7 @@ import classNames from "classnames";
 import SuperadminText from "./SuperadminText";
 import Portfolio from "./Portfolio";
 import { messapiaBold } from "./fonts";
+import Link from "next/link";
 
 export default async function Home() {
   const basics = await (
@@ -13,7 +14,7 @@ export default async function Home() {
   ).json();
 
   return (
-    <div className="grid sm:grid-cols-[340px,1fr]">
+    <div className="grid sm:grid-cols-[340px,1fr] sm:gap-2">
       <div className="p-6 pt-12 sm:pt-24 sm:sticky sm:top-0 w-full sm:h-screen">
         <h1
           className={classNames(
@@ -26,6 +27,19 @@ export default async function Home() {
 
         <div className="mt-4">
           <SuperadminText text={basics.bio} />
+        </div>
+
+        <div className="mt-10">
+          <Link
+            href="/"
+            className={classNames(
+              "block border border-gray-500 px-4 py-2 rounded-full text-gray-50 bg-gray-900",
+              "hover:bg-gray-50 hover:text-gray-900 hover:border-gray-50 transition-colors duration-200",
+              "font-medium text-center"
+            )}
+          >
+            Request Commission
+          </Link>
         </div>
       </div>
 
