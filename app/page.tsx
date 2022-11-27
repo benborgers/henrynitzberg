20271f7b8049eedbdaf4c3f36f342741033f65c9
Ben Borgers <borgersbenjamin@gmail.com>
2022-11-26 22:04:22 -0500

    Add profile photo

--- app/page.tsx
+++ app/page.tsx
@@ -15,10 +15,17 @@ export default async function Home() {
 
   return (
     <div className="grid sm:grid-cols-[340px,1fr] sm:gap-2">
-      <div className="p-6 pt-12 sm:pt-24 sm:sticky sm:top-0 w-full sm:h-screen">
+      <div className="px-4 pb-8 pt-8 sm:p-6 sm:pt-16 sm:sticky sm:top-0 w-full sm:h-screen">
+        {/* eslint-disable @next/next/no-img-element */}
+        <img
+          src={basics.image}
+          alt="Henry Nitzberg"
+          className="h-28 rounded-full"
+        />
+
         <h1
           className={classNames(
-            "text-gray-50 text-2xl font-bold",
+            "mt-4 text-gray-50 text-2xl font-bold",
             messapiaBold.className
           )}
         >
@@ -29,12 +36,12 @@ export default async function Home() {
           <Prose html={basics.bio} />
         </div>
 
-        <div className="mt-10">
+        <div className="mt-8 sm:mt-10">
           <Button href="/contact">Request Commission</Button>
         </div>
       </div>
 
-      <div className="p-4 pb-24 sm:pt-6 sm:pr-6 sm:pl-0">
+      <div className="p-4 pb-36 sm:pt-6 sm:pr-6 sm:pl-0">
         <Portfolio portfolio={portfolio} />
       </div>
     </div>
