28b363afac6cb0fca0e7365f05282a146a431928
Ben Borgers <borgersbenjamin@gmail.com>
2022-11-26 22:15:17 -0500

    Add Fathom

--- app/BaseHead.tsx
+++ app/BaseHead.tsx
@@ -1,3 +1,5 @@
+import Script from "next/script";
+
 const TITLE = "Henry Nitzberg";
 
 export default function BaseHead({ title = undefined }: { title?: string }) {
@@ -6,6 +8,14 @@ export default function BaseHead({ title = undefined }: { title?: string }) {
       <title>{title ? title + " - " + TITLE : TITLE}</title>
       <meta content="width=device-width, initial-scale=1" name="viewport" />
       <link rel="icon" href="/favicon.png" />
+
+      <Script
+        src="https://owl.benborgers.com/script.js"
+        data-site="IDHNIEZW"
+        data-included-domains="henrynitzberg.com"
+        data-spa="auto"
+        defer
+      />
     </>
   );
 }
