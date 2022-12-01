cfac8bc1a5538ca3e678189e22fc6cf78c8e355f
Ben Borgers <borgersbenjamin@gmail.com>
2022-12-01 15:44:57 -0500

    Add og:images

--- app/[slug]/head.tsx
+++ app/[slug]/head.tsx
@@ -1,8 +1,14 @@
 import BaseHead from "../BaseHead";
+import cdn from "../cdn";
 import getPortfolioEntry from "./getPortfolioEntry";
 
 export default async function Head({ params }: { params: { slug: string } }) {
   const entry = await getPortfolioEntry(params.slug);
 
-  return <BaseHead title={entry?.name} />;
+  return (
+    <>
+      <BaseHead title={entry?.name} />
+      {entry?.image && <meta name="og:image" content={cdn(entry.image, 800)} />}
+    </>
+  );
 }
--- app/head.tsx
+++ app/head.tsx
@@ -1,5 +1,15 @@
 import BaseHead from "./BaseHead";
 
 export default function Head() {
-  return <BaseHead />;
+  return (
+    <>
+      <BaseHead />
+      <meta
+        property="og:image"
+        content={`https://v1.screenshot.11ty.dev/${encodeURIComponent(
+          "https://henrynitzberg.com"
+        )}/opengraph/_${new Date().toISOString().split("T")[0]}`}
+      />
+    </>
+  );
 }
