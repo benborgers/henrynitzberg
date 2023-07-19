b7668d167b40cfcf0d5e515102ca896fe6fed29b
Ben Borgers <borgersbenjamin@gmail.com>
2023-07-18 23:19:55 -0400

    Try to fix issue where slug pages don't work

--- app/(pages)/[slug]/page.tsx
+++ app/(pages)/[slug]/page.tsx
@@ -2,10 +2,34 @@ import Link from "next/link";
 import { notFound } from "next/navigation";
 import { Metadata } from "next";
 import classNames from "classnames";
+import { createReader } from "@keystatic/core/reader";
 import cdn from "../cdn";
 import Prose from "../Prose";
 import XIcon from "../XIcon";
-import getPortfolioEntry from "./getPortfolioEntry";
+import PortfolioEntry from "../portfolio_entry";
+import config from "../../../keystatic.config";
+
+const reader = createReader(process.cwd(), config);
+
+const getPortfolioEntry = async (slug: string) => {
+  const order = (await reader.singletons.portfolioOrder.read())!.order;
+
+  const portfolio: PortfolioEntry[] = [];
+
+  for (const slug of order) {
+    const item = await reader.collections.portfolio.read(slug!);
+    if (item !== null && slug !== null && item.image !== null) {
+      portfolio.push({
+        ...item,
+        slug,
+        image: item.image,
+        caption: await item.caption(),
+      });
+    }
+  }
+
+  return portfolio.find((entry) => entry.slug.toString() === slug);
+};
 
 type Params = { slug: string };
 
